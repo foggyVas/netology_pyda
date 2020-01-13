f5b53c9178a92c40db01e0669a0e1ee1ec304de1
Alexey foggy <56228969+foggyVas@users.noreply.github.com>
2020-01-13 18:58:44 +0300

    Update ex1.py

--- part1/hw3/ex1.py
+++ part1/hw3/ex1.py
@@ -4,4 +4,4 @@ stats = {"facebook": 55, "yandex": 120, "vk": 115, "google": 99, "email": 42, "o
 reverse_dict = {v: k for k, v in stats.items()}
 print(reverse_dict[sorted(stats.values(), key=lambda x: x)[-1]])
 
-print(sorted(stats.items(), key=lambda x: -x[1])[0][0])
+# print(sorted(stats.items(), key=lambda x: -x[1])[0][0])
