7d263e3feea571b096c1803dc4f0857fe41b8bc5
Alexey foggy <56228969+foggyVas@users.noreply.github.com>
2020-01-17 17:30:42 +0300

    Update Ex2.py

--- part1/hw2/Ex2.py
+++ part1/hw2/Ex2.py
@@ -8,6 +8,15 @@ ids = [['user1', [213, 213, 213, 15, 213]],
        ['user2', [54, 54, 119, 119, 119]],
        ['user3', [213, 98, 98, 35]]
        ]
-print(ids[0][1])
-print(ids[1][1])
-print(ids[2][1])
+
+ids_sorted = list()
+
+
+for user in range(len(ids)):
+       for i in range(len(ids[user][1])):
+              if str(ids[user][1][i]).isdigit():
+                     if (ids[user][1][i]) in ids_sorted:
+                            pass
+                     else:
+                            ids_sorted.append(ids[user][1][i])
+print(ids_sorted)
